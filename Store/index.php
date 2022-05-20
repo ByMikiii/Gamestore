e138bad2ff127e868e420515dce929ee9e5ecd1c
ByMikiii <75531503+ByMikiii@users.noreply.github.com>
2022-05-21 01:12:34 +0200

    Avatars working 😋

--- Store/index.php
+++ Store/index.php
@@ -8,8 +8,13 @@ require '../scripts/getAllGames.php';
 <main class="flex flex-wrap justify-center mt-20 mx-28 ">
   <?php
   foreach ($games as $game) : ?>
-  <section class="text-gray-300  bg-slate-900 w-80 h- m-6 rounded-md overflow-hidden">
-    <img src="/Gamestore/Images/Games/Doom Eternal.jpeg" alt="" class="relative">
+  <?php if(strpos($game['name'], ':')!== false) {
+    $avatar = str_replace(":", "-", $game['name']);
+  }else {
+    $avatar = $game['name'];
+  }?>
+  <section class="text-gray-300  bg-slate-900 w-80 m-8 rounded-md overflow-hidden">
+    <img class="h-44 w-80" src="/Gamestore/Images/Games/<?php echo $avatar?>.jpeg" alt="" class="relative">
     <div class="flex h-16">
       <span class="text-xl font-semibold my-auto mx-1 px-2 "><?php echo $game['name'] ?></span>
       <span class="text-lg ml-auto mr-3 my-auto">
@@ -17,13 +22,12 @@ require '../scripts/getAllGames.php';
         if($game['original_price'] > 0){
           echo $game['original_price']."$";
         } else{
-          echo "FREE";}?></span>
+          echo "<span class='text-green-500'>FREE</span>";}?></span>
     </div>
   </section>
   <?php 
   endforeach; ?>
 </main>
-
 <?php 
 include '../parts/footer.php';
 ?>
--- parts/footer.php
+++ parts/footer.php
@@ -1,3 +1,4 @@
+<footer class="m-2 mt-12">Ⓒ2022 ByMikiii</footer>
 </body>
 
 </html>
